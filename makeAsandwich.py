33a7cd9ae7124a6402d958ad83d0a1cc863dd2d1
PramishBytes <pramishadhikari30@gmail.com>
2026-04-22 14:09:25 +0545

    PythonChallenge: Make a Sandwich

---
+++ makeAsandwich.py
@@ -0,0 +1,41 @@
+r'''
+Functional code challenge: Make a sandwich
+
+Task:
+
+Create a Python function named make_sandwich.
+
+This function should accept the following parameters:
+
+bread_type (a string representing the type of bread)
+
+filling (a string for the main sandwich filling)
+
+cheese (an optional string for the cheese type, defaulting to "none")
+
+toasted (an optional boolean indicating if the sandwich is toasted, defaulting to False)
+
+Inside the function, construct a descriptive sentence about the sandwich being made, incorporating all the provided details.
+
+Have the function return the sentences shown in the Expected Output section below
+
+
+Tips:
+
+Remember to use default values for the optional parameters.
+
+Use two if statements to handle the cheese case and the toasted case.
+
+You can use f-strings for convenient string formatting.
+
+Ensure you return the string, not print it.
+
+
+Example Input:
+
+make_sandwich("wheat", "turkey", "cheddar", True)
+make_sandwich("rye", "ham")
+
+
+'''
+
