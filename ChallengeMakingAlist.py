b30749a1c9d7dd0e453edf4b7c9538f53aaffeda
PramishBytes <pramishadhikari30@gmail.com>
2026-04-23 22:05:07 +0545

    Python: Challenge for making a list

---
+++ ChallengeMakingAlist.py
@@ -0,0 +1,23 @@
+r'''
+
+Code challenge: Making a list
+Your Task:
+Write the Python code required to take a list of numbers as input and return a new list containing only the unique numbers from the original list, maintaining their original order, and then print the result.
+
+Tips:
+Sets are designed to store unique elements.
+
+Use set() to only get the unique numbers from the original array.
+
+Save the results in the unique_set variable.
+
+Example input:
+Python
+
+numbers = [1, 2, 2, 3, 1, 4, 5, 3]
+
+Expected output:
+[1, 2, 3, 4, 5]
+
+'''
+
